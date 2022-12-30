36ea4d6b8efafb24b5b892e31dcb9d35b11af4c0
Vladislav <vlad.shaldymov@mail.ru>
2022-12-30 18:29:57 +0500

    refactor code

--- game/src/main/java/com/rgs/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rgs.repository;
-
-import com.rgs.entity.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
